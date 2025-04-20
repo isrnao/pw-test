f93f5b83467964cdcc594fe6e254f421932b65e5
isrnao <jggpad@gmail.com>
2025-04-21 07:55:30 +0900

    wip

--- tests/ysearch.spec.ts
+++ tests/ysearch.spec.ts
@@ -6,5 +6,5 @@ test('YahooからGoogleへの遷移テスト', async ({ page }) => {
 
   await page.getByRole('searchbox', { name: '検索したいキーワードを入力してください' }).fill('google');
   await page.getByRole('searchbox', { name: '検索したいキーワードを入力してください' }).press('Enter');
-  await page.getByRole('link', { name: 'Google https://www.google.co.' }).click();
+  await page.getByRole('link', { name: /Google.*google\./ }).first().click();
 });
